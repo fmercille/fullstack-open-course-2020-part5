4e8cfaaba76d80d11768326e9e52a3e564451281
Frederic Mercille <fmercille@gmail.com>
2020-08-02 17:53:33 -0400

    Exercise 5.7

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,38 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [expanded, setExpanded] = useState(false)
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  let expandedInfo = ''
+  if (expanded) {
+    expandedInfo = (
+      <>
+        <div>
+          {blog.url}
+        </div>
+        <div>
+          Likes {blog.likes} <button onClick={() => console.log('Like')}>like</button>
+        </div>
+        <div>
+          {blog.user.name}
+        </div>
+      </>
+    )
+  }
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author} <button onClick={() => setExpanded(!expanded)}>{expanded ? 'Hide' : 'Show'}</button>
+      {expandedInfo}
+    </div>
+  )
+}
 
 export default Blog
